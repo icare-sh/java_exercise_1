4cbc1139fbeb0cae660015d8f78c18f2274762c2
Sabir <sabir.mba@protonmail.com>
2022-02-09 19:23:12 +0100

    Launcher Loop done

--- src/Launcher.java
+++ src/Launcher.java
@@ -3,13 +3,15 @@ import java.util.Scanner;
 public class Launcher {
     public static void main(String [] args) {
         System.out.println("Welecome to the best program of the world!!");
-        Scanner scanner = new Scanner(System.in);
-        String str = scanner.nextLine();
-        if (str.equals("quit")) {
-            System.out.println("command " + str);
-        }
-        else {
-            System.out.println("Unknown command");
+        while(true) {
+            Scanner scanner = new Scanner(System.in);
+            String str = scanner.nextLine();
+            if (str.equals("quit")) {
+                break; 
+            }
+            else {
+                System.out.println("Unknown command");
+            }
         }
     }
 }
