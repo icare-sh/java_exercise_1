4eb93d0245a77c810589b1bc32acec604f3c7afd
Sabir <sabir.mba@protonmail.com>
2022-02-09 23:03:54 +0100

    Launcher second command done

--- src/Launcher.java
+++ src/Launcher.java
@@ -1,7 +1,16 @@
 import java.util.Scanner;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.Files;
+import java.io.IOException;
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.*;
 
 public class Launcher {
-    public static void main(String [] args) {
+    public static void main(String [] args) 
+        throws IOException
+    {
         System.out.println("Welecome to the best program of the world!!");
         while(true) {
             Scanner scanner = new Scanner(System.in);
@@ -27,8 +36,38 @@ public class Launcher {
                 }
 
             }
+            else if (str.equals("freq")) {
+                System.out.println("Donnez le chemin du fichier: ");
+                Scanner p = new Scanner(System.in);
+                String pt = p.nextLine();
+                Path path =  Paths.get(pt);
+
+                String s = Files.readString(path);
+                String newStr = s.replaceAll("\\p{Punct}", " ");
+                String[] words = newStr.toLowerCase().split("\\s+");
+                Stream<String> stream = Arrays.stream(words);
+
+                Map<String, Long> result = stream.collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+                Map<String, Long> finalMap = new LinkedHashMap<>();
+
+                result.entrySet().stream().sorted(Map.Entry.<String, Long>comparingByValue().reversed()).forEachOrdered(e -> finalMap.put(e.getKey(), e.getValue()));
+
+                int i = 0;
+                for (Map.Entry<String, Long> entry : finalMap.entrySet()) {
+                    String val = entry.getKey();
+                    System.out.print(val);
+                    i++;
+                    if (i == 3) {
+
+                        System.out.print("\n");
+                        break;
+                    }
+                    System.out.print(" ");
+                }
+            }
             else {
                 System.out.println("Unknown command");
+
             }
         }
     }
