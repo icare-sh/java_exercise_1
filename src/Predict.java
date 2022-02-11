0abea60d03e3596ce89c4aba5339e86764374c9b
Sabir <sabir.mba@protonmail.com>
2022-02-11 10:53:30 +0100

    Launcher predict v3

--- src/Predict.java
+++ src/Predict.java
@@ -1,51 +1,62 @@
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.LinkedHashMap;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
-public class Predict implements Command {
-
-    static void readFile(String file, String mot) {
-        try {
-            Path filePath = Paths.get(file);
-            String content = Files.readString(filePath);
-            String[] lines = content.split(" ");
-            boolean founded = false;
-            for (String word : lines)
-            {
-                if (word.equals(mot)) {
-                    founded = true;
-                }
-            }
-            if (!founded) {
-                System.out.println("Mot non trouvé");
-            }
-            else {
-                for (String word : lines)
-                {
-                    System.out.print(word + " ");
-                }
-                System.out.println("");
-            }
-        } catch(Exception e) {
-            System.out.println("Unreadable file: " + e.getClass() + " " +  e.getMessage());
-        }
-    }
-
+public class Predict implements Command{
     @Override
     public String name() {
         return "predict";
     }
 
     @Override
-    public boolean run(Scanner sc) {
-        System.out.println("Fichier :");
-        String file = sc.nextLine();
-        System.out.println("Mot :");
-        String mot = sc.nextLine();
-        readFile(file, mot);
+    public boolean run(Scanner scanner) {
+        System.out.println("Entrer le chemin du fichier");
+        String pathString = "";
+
+        try {
+            pathString = scanner.nextLine();
+            String text = Files.readString(Paths.get(pathString));
+            text = text.replaceAll("[.!?\\-,\\n]", " ").toLowerCase(Locale.ROOT);
+            List<String> tab = List.of(text.split(" "));
+
+            System.out.println("Entrer un mot");
+            String word = scanner.nextLine();
+            word = word.toLowerCase(Locale.ROOT);
+
+            if (!tab.contains(word)) {
+                System.out.println("Le mot n'est pas présent dans le fichier");
+                return false;
+            }
+
+            StringJoiner sentence = new StringJoiner(" ");
+            sentence.add(word);
+            for (int j = 0; j < 19; j++) {
+                List<String> next = new ArrayList<String>();
+                for (int i = 0; i < tab.size() - 1; i++) {
+                    if (tab.get(i).equals(word)) {
+                        next.add(tab.get(i + 1));
+                    }
+                }
+
+                Map<String, Long> next_list = next.stream().collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+                Map<String, Long> result = next_list.entrySet().stream()
+                        .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+                        .limit(1)
+                        .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (oldValue, newValue) -> oldValue, LinkedHashMap::new));
+                List<String> list3 = new ArrayList<>(result.keySet());
+                String str = list3.get(0);
+                sentence.add(str);
+                word = str;
+            }
+            System.out.println(sentence);
+        } catch (IOException e) {
+            System.out.println("Unreadable file: " + e.getClass() + " " + e.getMessage());
+            return false;
+        }
         return true;
     }
 }
